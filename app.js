adb6202e08e6c05e73865849d907d56f3138d9cd
Jhairolopez23 <jhonatanlopezr0203@gmail.com>
2025-08-11 20:57:32 -0500

    Creación de la funcion para capturar el nombre ingresado, verificar, añadirlo al array y luego limpiar el campo de texto.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosIngresados = [];
+
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById("amigo").value;
+    if(amigoIngresado == ""){
+        alert('Ingrese un nombre');
+    }else{
+        //Añadimos el amigo al arreglo.
+        amigosIngresados.push(amigoIngresado);
+
+        //Limpiamos el campo de texto.
+        document.getElementById("amigo").value = "";
+    }
+}
